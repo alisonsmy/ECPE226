1f74a427f7189f901db5b2d48baeef38c032ef8c
alisonsmy <m_shu@u.pacific.edu>
2017-09-26 15:48:31 -0700

    create .m file and add .mat file

---
+++ ECPE226HW3.m
@@ -0,0 +1,19 @@
+% Alison Shu, Jeffrey Dwyer
+% Dr. Mueller
+% ECPE 226
+% 9/25/2017
+% HW3 - Logistic Regression
+
+
+File = load('usps_modified.mat');
+raw_data = File.data;
+[img, example, digit] = size(raw_data);
+
+% Logistic Regression with Batch Gradient Descent
+
+
+
+
+
+
+% Logistic Regression with Stochastic Gradient Descent (single data point)
