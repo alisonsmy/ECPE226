dd78b64ca666a37bc7b804cff1a366c94b6956d8
alisonsmy <m_shu@u.pacific.edu>
2017-09-28 20:12:18 -0700

    update stochastic func

---
+++ StochasticGradientDescent.m
@@ -0,0 +1,30 @@
+function [ result ] = StochasticGradientDescent(training, learningRate)
+
+[d, N] = size(training);
+
+disp("Dimension is: " + d);
+disp("Training  size: " + N);
+
+run = true;
+
+x = training(2:d, :);
+y = training(1, :);
+
+% d-1 because the first value is the result
+w = zeros(d-1,1);
+while run
+    i = randi([1 N], 1); %get a random integer from 1 to N
+    grad = GradientSignal(w, x(:,i), y(:,i)); 
+
+    %update weight vector
+    w = w - (learningRate)*grad; 
+
+    % Determine if algorithm should stop
+    run = norm(grad) < 0.1; % if gradient is small enough
+
+end
+
+result = w;
+
+end
+
