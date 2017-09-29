a86e93a155462689a6a2dc314115a8b6404d65bd
Jeff <jeffreydwyer95@hotmail.com>
2017-09-29 08:54:04 -0700

    Changed error calc

--- MeasureError.m
+++ MeasureError.m
@@ -3,20 +3,20 @@ function [ inError, outError ] = MeasureError( training, testing, w)
 %   Detailed explanation goes here
 [d, N] = size(training);
 [~, M] = size(testing);
-errors = zeros(M,1);
+errors = 0;
 
 for i = 1:N
     s = dot(w, training(2:d,i));
-    errors(i) = (training(1,i) - s)^2;
+    errors = errors + (training(1,i) - s)^2;
 end
-inError = mean(errors);
+inError = errors / (2*N);
 
-errors = zeros(M,1);
+errors = 0;
 for i = 1:M
     s = dot(w, testing(2:d,i));
-    errors(i) = (testing(1,i) - s)^2;
+    errors = errors + (testing(1,i) - s)^2;
 end
-outError = mean(errors);
+outError = errors / (2*M);
 
 end
 
