9df90156998da413f0d6084ec9f3fcac132baf1f
Jeff <jeffreydwyer95@hotmail.com>
2017-09-29 07:52:31 -0700

    Cleaned up a bit

--- ECPE226HW3.m
+++ ECPE226HW3.m
@@ -4,7 +4,6 @@
 % 9/25/2017
 % HW3 - Logistic Regression
 
-
 File = load('usps_modified.mat');
 raw_data = File.data;
 [img, N, digit] = size(raw_data);
@@ -20,31 +19,6 @@ cleanedData = cleanedData(:,idx);
 testing = cleanedData(:,1:400);
 training = cleanedData(:,400:(N*digit));
 
-% training = raw_data(:,:,1);
-% 
-% % Get Intensity and symmetry from training set
-% %get x1 from raw data. x1 is intensity
-% x1 = GetIntensity(training); % intensity of digit #, which is x1
-% 
-% % get x2 from raw data. x2 is symmetry
-% x2 = GetSymmetry(training);  % symmetry of digit #, which is x2
-% 
-% VecX3 = [];
-% VecX4 = [];
-% for i = 2:10
-%     verification = raw_data(:,:,i);
-%     x3 = GetIntensity(verification);
-%     x4 = GetSymmetry(verification);
-%     VecX3 = [VecX3; x3];
-%     VecX4 = [VecX4; x4];
-% end
-% 
-% % put constant (1) x's first column
-% Const = ones(N, 1);
-% xTrain = [Const, x1, x2];
-% Const1 = ones(N*9, 1);
-% xVerif = [Const1, VecX3, VecX4];
-
 lr = 0.1; % learning rates of 0.1, 1, 10, 50.
 
 figure;
@@ -54,29 +28,22 @@ ax = gca;
 ax.XAxisLocation = 'origin';
 ax.YAxisLocation = 'origin';
 scatter(ones(3,:), ones(4,:),5,'g','filled');
-
 scatter(others(3,:), others(4,:),5,'r','filled');
 hold off;
+
 % Run 10 times each for each rate
 for r = 1:10
-    % result = BatchGradientDescent(testing, training, lr);
+%     result = BatchGradientDescent(training, testing, lr);
     result = StochasticGradientDescent(training, lr);
 
-    % disp(result);
-    % 
-    % disp(ones(:, 50));
-
-    % scatter(ones(3,:), ones(4,:),5,'g','filled');
-    % hold('on');
-    % 
-    % scatter(others(3,:), others(4,:),5,'r','filled');
-    % hold('on');
-
-    % disp("Result is " + result)
     m = -1:0.01:1;
     k = -(result(2)*m + result(1))/result(3);
     hold on;
-    plot(m,k, 'color',[.9 .9 .9]);
+    if r == 10
+        plot(m,k, 'color',[.9 0.5 0.0]);
+    else
+        plot(m,k, 'color',[.9 .9 .9]);
+    end
     hold off;
 end
 
--- GetIntensity.m
+++ GetIntensity.m
@@ -1,14 +1,17 @@
 % Vector with each average intensity value in the image
 function intensity = GetIntensity(D)
-%      [r] = size(D); % r is 256, c is 500
-r = 256;
-%     Total_intensity = zeros(c, 1); % row vector with size of 500
-%     for j = 1:c % j is 1 to 500
-pixel = D; % get pixel of example 1
-data = 2*(double(pixel)/255)-1; % convert pixels with range from -1 to 1
-sumOfM = sum(data); % sum of pixels in martix
-intensity = sumOfM(1)/r;    % intensity = sum/256
-%         Total_intensity(j, 1) = intensity; % put intensity into intensity vector
-%     end
-    return
+
+[r, ~] = size(D);
+
+% get pixel of example 1
+pixel = D; 
+
+% convert pixels with range from -1 to 1
+data = 2*(double(pixel)/255)-1; 
+
+% sum of pixels in martix
+sumOfM = sum(data); 
+
+% intensity = sum/256
+intensity = sumOfM(1)/r;    
 end
