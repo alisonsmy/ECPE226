caa3873a8473b160ad20a89682393894819408ac
alisonsmy <m_shu@u.pacific.edu>
2017-10-20 12:05:36 -0700

    Updated FWD Prop.

--- neuralnetwork/ForwardPropagation.m
+++ neuralnetwork/ForwardPropagation.m
@@ -2,17 +2,20 @@ function [ result ] = ForwardPropagation(w, x, y)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
     X = x(0,:);
-    L = length(X);
-    [n, d] = size(x);
+    [n, d] = size(X);
     s = zeros(n, d);
-    for l = 1:L
-        s(l, :) = w(1, :)*X(l-1, :);
-        theta = transformation(s(l, :));
-        x(l, :) = [1, theta];
+    for l = 1:n
+        s(l, :) = dot(w(1, :), X(l-1, :), 2);
+        theta = transformation(s);
+        X(l, :) = [1, theta];
     end
     result = X(L);
 end
 
-function [ t ] = transformation(s)
-    t = 1/(1+e^(-s));
+function [ theta ] = transformation(s)
+    [n, d] = size(s);
+    theta = zeros(n, d);
+    for i = 1:n
+        theta(i) = 1/(1+exp(y(i)*s(i)));    %calculate theta(-s)
+    end
 end
