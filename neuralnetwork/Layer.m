67ad039b2dae7c51a6f72a5ae96b72af86fd5f39
Jeffrey Dwyer <jdwyer4@STK.PACIFIC.EDU>
2017-10-20 18:37:34 -0700

    Transcribed forward and back propogation from slides

--- neuralnetwork/Layer.m
+++ neuralnetwork/Layer.m
@@ -1,14 +1,23 @@
-classdef Layer
+classdef Layer < handle
     properties
         weights
         outputs
+        theta
+        thetaPrime
+        weightUpdate
     end
     
     methods
-        function forward(prev)
+        function h = forward(obj, xPrev)
+            s = dot(obj.weights, xPrev);
+            obj.outputs = [1 obj.theta(s)];
+            h = obj.outputs;
         end
         
-        function back(prev)
+        function deltas = back(obj, deltasPrev)
+            sPrime = obj.thetaPrime(obj.outputs(2:end));
+            deltas = sPrime .* dot(obj.weights,deltasPrev);
+            obj.weights = obj.weightUpdate(obj.weights, deltas);
         end
     end
 end
