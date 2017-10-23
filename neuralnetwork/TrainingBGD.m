b428e9b7462e3daa4bfe42c3f8c9930f233d511b
Jeffrey Dwyer <jdwyer4@STK.PACIFIC.EDU>
2017-10-22 21:23:32 -0700

    Made fixes to batch

--- neuralnetwork/TrainingBGD.m
+++ neuralnetwork/TrainingBGD.m
@@ -2,18 +2,18 @@ function [g, Ein] = TrainingBGD(network,y)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
     [L, ~] = size(network);
-    [n, d] = size(network(1).outputsTheta);
+    [n, d] = size(network(1).outputThetas);
     Ein = 0;
     G = zeros(n, d+1);
     g = zeros(L, d+1);
     for i = 1:n
         %do FWD Prop
         for l = 1:L
-            input = network(l).forward(network(1).outputsTheta(i,:));
+            input = network(l).forward(network(1).outputThetas(i,:));
         end
         %do Back Prop
         x = network(3).outputs(:,1);
-        deltas = (x - y(i)) * thetaPrime(network(3).outputThetas);
+        deltas = (x - y(i)) * network(3).thetaPrime(network(3).outputs);
         for l = L:-1:1
             deltas = network(l).back(deltas);
         end
@@ -21,14 +21,15 @@ function [g, Ein] = TrainingBGD(network,y)
         Ein = Ein + (1/(2*n))*((input-y(i))^2);
         %Calculate gradient
         for l = 1:L
-            G(i,:) = network(l-1).outputsTheta*deltas';
+            G(i,:) = network(l-1).outputThetas*deltas';
             G = G + (1/n)*G(i, :);
             %g(l, :) = G;
         end
     end
-    %v = (sum(g)/n)';                %get total gradient vector
-    for l = 1:L
-       network(l).weights = network(l).weightUpdate(network(l).weights, G);
+
+    deltas = G;
+    for i = 3:-1:1
+        deltas = network(i).back(deltas, 0.1);
     end
     
 end
