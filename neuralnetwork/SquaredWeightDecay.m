237c88b9aadd2deed64d5fc328f4f1dbb81736c9
alisonsmy <m_shu@u.pacific.edu>
2017-11-01 13:13:06 -0700

    Weight Decay is done

--- neuralnetwork/SquaredWeightDecay.m
+++ neuralnetwork/SquaredWeightDecay.m
@@ -4,7 +4,6 @@ function [Eaugs] = SquaredWeightDecay(network, training)
     x = training(3:d, :);
     y = training(1, :);
     lambda = 0.01;
-    Eaugs = zeros(L, 1);
     Eaug = zeros(L, 1);
     sum = 0;
     for l = L:-1:2
@@ -20,8 +19,16 @@ function [Eaugs] = SquaredWeightDecay(network, training)
        Ein = TrainingSGD(network, x(:,i), y(i));
        Eaug(l, :) = Ein + get;
     end
-    for l = 1:L
-        Eaugs(l, :) = (Eaug(l, :)/network(l).weights);
+    for l = L:-1:2
+        deltas = network(l).backNoUpdate(Eaug);
+        xn = [1; network(l-1).outputThetas];
+        Gn = xn*deltas';
+        G = network(l).gradient;
+        network(l).gradient = G + Gn;
+        if any(isnan(network(l).gradient(:)))
+            frprintf('nans on line 29 SquaredWeightDecay!');
+        end
     end
+    Eaugs = deltas;
 end
 
