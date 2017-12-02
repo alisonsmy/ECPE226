3e62435c17096f962ea59a75e15ce26c77583502
alisonsmy <m_shu@u.pacific.edu>
2017-12-02 15:18:23 -0800

    added prop selection method

--- HW4/Proportional.m
+++ HW4/Proportional.m
@@ -1,7 +1,21 @@
-function [outputArg1,outputArg2] = Proportional(inputArg1,inputArg2)
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function [ newcand, I ] = Proportional( candidate, parents )
+   [~,I] = sort(candidate.fit,'ascend');
+   total = sum(candidate.fit);
+   [m, n] = size(I);
+   prop = zeros(m, n);
+   index = zeros(m, n);
+   for i = 1:I
+      prop(i) = candidate.fit(i)/total;
+   end
+   pick = randi([0 1], 1, 1);
+   j = 1;
+   for i = 1:I
+       if prop(i) >= pick
+          index(j) = i;
+          j = j + 1;
+       end
+   end
+   newcand.binx(1:parents,:) = candidate.binx(index(1:parents),:);
+   newcand.biny(1:parents,:) = candidate.biny(index(1:parents),:);
 end
 
