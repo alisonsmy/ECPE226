95a483c7942b12855b85da211750ee2aaaad1050
alisonsmy <m_shu@u.pacific.edu>
2017-12-02 16:51:39 -0800

    comment out tourament with replacement part

--- HW4/Tourament.m
+++ HW4/Tourament.m
@@ -19,23 +19,24 @@ function [newcand, I] = Tourament( candidate, parents )
         else
             newcand.biny(i,:) = candidate.biny(I(pick2),:);
         end
+        
         % with replacement
         
-        if candidate.binx(I(pick1),:) >= candidate.binx(I(pick2),:)
-            newcand.binx(i,:) = candidate.binx(I(pick1),:);
-            candidate.binx(I(pick1),:) = [];
-        else
-            newcand.binx(i,:) = candidate.binx(I(pick2),:);
-            candidate.binx(I(pick2),:) = [];
-        end
-        
-        if candidate.biny(I(pick1),:) >= candidate.biny(I(pick2),:)
-            newcand.biny(i,:) = candidate.biny(I(pick1),:);
-            candidate.biny(I(pick1),:) = [];
-        else
-            newcand.biny(i,:) = candidate.biny(I(pick2),:);
-            candidate.biny(I(pick2),:) = [];
-        end
+%         if candidate.binx(I(pick1),:) >= candidate.binx(I(pick2),:)
+%             newcand.binx(i,:) = candidate.binx(I(pick1),:);
+%             candidate.binx(I(pick1),:) = [];
+%         else
+%             newcand.binx(i,:) = candidate.binx(I(pick2),:);
+%             candidate.binx(I(pick2),:) = [];
+%         end
+%         
+%         if candidate.biny(I(pick1),:) >= candidate.biny(I(pick2),:)
+%             newcand.biny(i,:) = candidate.biny(I(pick1),:);
+%             candidate.biny(I(pick1),:) = [];
+%         else
+%             newcand.biny(i,:) = candidate.biny(I(pick2),:);
+%             candidate.biny(I(pick2),:) = [];
+%         end
     end
 end
 
